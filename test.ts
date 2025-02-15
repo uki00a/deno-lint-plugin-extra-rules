c9759d33dbafb492119138fbecb8847cecf3cea7
uki00a <uki00a@gmail.com>
2025-02-15 15:02:52 +0900

    feat: `valid-expect` rule

--- test.ts
+++ test.ts
@@ -2,6 +2,11 @@ import { assertObjectMatch } from "@std/assert/object-match";
 import { assertStrictEquals } from "@std/assert/strict-equals";
 import { createPlugin } from "./plugin.ts";
 
+interface TestCase {
+  filename: string;
+  expected: Array<Partial<Deno.lint.Diagnostic>>;
+}
+
 async function runLintPlugin(
   plugin: Deno.lint.Plugin,
   filename: string,
@@ -15,68 +20,104 @@ async function runLintPlugin(
   );
 }
 
-Deno.test("plugin", async (t) => {
-  await t.step("no-env-to-object", async () => {
-    const plugin = createPlugin();
-    const diagnostics = await runLintPlugin(plugin, "no-env-to-object.js");
-    assertObjectMatch(diagnostics[0], {
-      id: "deno-lint-plugin-extra-rules/no-env-to-object",
-      message: "`Deno.env.toObject()` requires full `--allow-env` permission.",
-    });
-    assertStrictEquals(diagnostics.length, 1);
+Deno.test("no-env-to-object", async () => {
+  const plugin = createPlugin();
+  const diagnostics = await runLintPlugin(plugin, "no-env-to-object.js");
+  assertObjectMatch(diagnostics[0], {
+    id: "deno-lint-plugin-extra-rules/no-env-to-object",
+    message: "`Deno.env.toObject()` requires full `--allow-env` permission.",
   });
+  assertStrictEquals(diagnostics.length, 1);
+});
 
-  await t.step("no-test-sanitizers", async (t) => {
-    const plugin = createPlugin();
-    const tests: Array<
-      { filename: string; expected: Array<Partial<Deno.lint.Diagnostic>> }
-    > = [
-      {
-        filename: "resources.js",
-        expected: [
-          {
-            id: "deno-lint-plugin-extra-rules/no-test-sanitizers",
-            message: "Disabling test sanitizers should be avoided.",
-            hint: "`sanitizeResources: true` should be removed.",
-          },
-        ],
-      },
-      {
-        filename: "ops.js",
-        expected: [
-          {
-            id: "deno-lint-plugin-extra-rules/no-test-sanitizers",
-            message: "Disabling test sanitizers should be avoided.",
-            hint: "`sanitizeOps: true` should be removed.",
-          },
-        ],
-      },
-      {
-        filename: "name-options-fn.js",
-        expected: [
-          {
-            id: "deno-lint-plugin-extra-rules/no-test-sanitizers",
-            message: "Disabling test sanitizers should be avoided.",
-            hint: "`sanitizeOps: true` should be removed.",
-          },
-        ],
-      },
-      {
-        filename: "ok.js",
-        expected: [],
-      },
-    ];
-    for (const { filename, expected } of tests) {
-      await t.step(filename, async () => {
-        const diagnostics = await runLintPlugin(
-          plugin,
-          `no-test-sanitizers/${filename}`,
-        );
-        for (let i = 0; i < expected.length; i++) {
-          assertObjectMatch(diagnostics[i], expected[i]);
-        }
-        assertStrictEquals(diagnostics.length, expected.length);
-      });
-    }
-  });
+Deno.test("no-test-sanitizers", async (t) => {
+  const plugin = createPlugin();
+  const tests: Array<TestCase> = [
+    {
+      filename: "resources.js",
+      expected: [
+        {
+          id: "deno-lint-plugin-extra-rules/no-test-sanitizers",
+          message: "Disabling test sanitizers should be avoided.",
+          hint: "`sanitizeResources: true` should be removed.",
+        },
+      ],
+    },
+    {
+      filename: "ops.js",
+      expected: [
+        {
+          id: "deno-lint-plugin-extra-rules/no-test-sanitizers",
+          message: "Disabling test sanitizers should be avoided.",
+          hint: "`sanitizeOps: true` should be removed.",
+        },
+      ],
+    },
+    {
+      filename: "name-options-fn.js",
+      expected: [
+        {
+          id: "deno-lint-plugin-extra-rules/no-test-sanitizers",
+          message: "Disabling test sanitizers should be avoided.",
+          hint: "`sanitizeOps: true` should be removed.",
+        },
+      ],
+    },
+    {
+      filename: "ok.js",
+      expected: [],
+    },
+  ];
+  for (const { filename, expected } of tests) {
+    await t.step(filename, async () => {
+      const diagnostics = await runLintPlugin(
+        plugin,
+        `no-test-sanitizers/${filename}`,
+      );
+      for (let i = 0; i < expected.length; i++) {
+        assertObjectMatch(diagnostics[i], expected[i]);
+      }
+      assertStrictEquals(diagnostics.length, expected.length);
+    });
+  }
+});
+
+Deno.test("valid-expect", async (t) => {
+  const plugin = createPlugin();
+  const tests: Array<TestCase> = [
+    {
+      filename: "ok.js",
+      expected: [],
+    },
+    {
+      filename: "matcher-not-specified.js",
+      expected: [
+        {
+          id: "deno-lint-plugin-extra-rules/valid-expect",
+          message: "A matcher is not specified.",
+        },
+      ],
+    },
+    {
+      filename: "matcher-not-called.js",
+      expected: [
+        {
+          id: "deno-lint-plugin-extra-rules/valid-expect",
+          message: "The matcher is not called.",
+        },
+      ],
+    },
+  ];
+  for (const { filename, expected } of tests) {
+    await t.step(filename, async () => {
+      const diagnostics = await runLintPlugin(
+        plugin,
+        `valid-expect/${filename}`,
+      );
+      assertStrictEquals(diagnostics.length, expected.length);
+      for (let i = 0; i < expected.length; i++) {
+        assertObjectMatch(diagnostics[i], expected[i]);
+      }
+    });
+  }
 });
---
+++ testdata/valid-expect/ok.js
@@ -0,0 +1,4 @@
+import { expect } from "@std/expect";
+
+expect(1).toBe(2);
+expect("foo").toStrictEqual("bar");
